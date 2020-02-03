840ea65dbf9839222ab800450bd6ce604b06dea6
ARUZENCE <Andrejs.Ruzencevs@dbschenker.com>
2020-02-03 16:21:00 -0500

    initial commit

---
+++ src/test/java/schenker/mailer/engine/EngineApplicationTests.java
@@ -0,0 +1,13 @@
+package schenker.mailer.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
